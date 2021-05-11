0a9f8e7fbffba9289ffca8f40acad26269194c67
SinisterBBT <polyakovworkbox@gmail.com>
2021-05-11 04:10:53 +0300

    making project structure correct allowing multiple subprojects inside the main one

--- pushImage.sh
+++ pushImage.sh
@@ -3,17 +3,9 @@ set -e
 
 imageTag=$1
 if [ -z "$1" ]
-  then
-    echo No image tag provided. latest will be used
-    imageTag=latest
+	then
+		echo No image tag provided. Latest will be used
+		imageTag=latest
 fi
 
-repositoryName=758121016477.dkr.ecr.us-east-2.amazonaws.com/mainapp
-imageFullName=$repositoryName:$imageTag
-
-echo [Main app STARTING] pushing image $imageFullName...
-
-echo [Main app] pushing image...
-docker push $imageFullName
-
-echo [Main app FINISHED] image $imageFullName pushed
+(exec "${BASH_SOURCE%/*}\mainApp\pushImage.sh" $imageTag)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "stringconcatcourse"
+include("mainApp")
